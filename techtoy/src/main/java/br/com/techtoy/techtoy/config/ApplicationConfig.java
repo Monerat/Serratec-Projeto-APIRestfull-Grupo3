50e17df20307b0c247bc9c736c53bdc33c8bab4a
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-10-16 23:04:18 -0300

    add: arquivos de estrutura para DTOs
    add: pastas dto e config
    add: arquivo de config para Mapper
    add: dependencia modelmapper
    change: modificado path do gitignore
    change: adicionado path env/ no gitignore

---
+++ techtoy/src/main/java/br/com/techtoy/techtoy/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package br.com.techtoy.techtoy.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public ModelMapper modelMaper(){
+        return new ModelMapper();
+    }
+}
