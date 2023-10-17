b0b195f6b387b2d4c409bccd5e35c28fab0c1a9f
Monerat <gmonerat@gmail.com>
2023-10-17 14:12:50 -0300

    add: DTO de produto

---
+++ techtoy/src/main/java/br/com/techtoy/techtoy/dto/produto/ProdutoRequestDTO.java
@@ -0,0 +1,26 @@
+package br.com.techtoy.techtoy.dto.produto;
+
+import java.util.List;
+
+import br.com.techtoy.techtoy.dto.categoria.CategoriaRequestDTO;
+import br.com.techtoy.techtoy.dto.pedidoItem.pedidoItemRequestDTO;
+
+public class ProdutoRequestDTO extends ProdutoBaseDTO {
+
+    private CategoriaRequestDTO categoria;
+    private List<PedidoItemRequestDTO> pedidoItens;
+
+    public CategoriaRequestDTO getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(CategoriaRequestDTO categoria) {
+        this.categoria = categoria;
+    }
+    public List<PedidoItemRequestDTO> getPedidoItens() {
+        return pedidoItens;
+    }
+    public void setPedidoItens(List<PedidoItemRequestDTO> pedidoItens) {
+        this.pedidoItens = pedidoItens;
+    }
+
+}
--- techtoy/src/main/java/br/com/techtoy/techtoy/service/ProdutoService.java
+++ techtoy/src/main/java/br/com/techtoy/techtoy/service/ProdutoService.java
@@ -1,11 +1,15 @@
 package br.com.techtoy.techtoy.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.techtoy.techtoy.dto.produto.ProdutoRequestDTO;
+import br.com.techtoy.techtoy.dto.produto.ProdutoResponseDTO;
 import br.com.techtoy.techtoy.model.Produto;
 import br.com.techtoy.techtoy.model.exceptions.ResourceNotFound;
 import br.com.techtoy.techtoy.repository.ProdutoRepository;
@@ -16,33 +20,48 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private ModelMapper mapper;
     //CRUD
 
     //Create
-    public Produto adicionar(Produto produto){
-        produto.setId(0);
-        return produtoRepository.save(produto);
+    public ProdutoResponseDTO adicionar(ProdutoRequestDTO produtoRequest){
+        
+        Produto produtoModel = mapper.map(produtoRequest, Produto.class);
+        produtoRepository.save(produtoModel);
+        return mapper.map(produtoModel, ProdutoResponseDTO.class);
     }
 
     //Read
-    public List<Produto> obterTodos(){
-        return produtoRepository.findAll();
+    public List<ProdutoResponseDTO> obterTodos(){
+        List<Produto> produtoModel = produtoRepository.findAll();
+        List<ProdutoResponseDTO> produtoResponse = new ArrayList<>();
+
+        for (Produto produto : produtoModel){
+            produtoResponse.add(mapper.map(produto, ProdutoResponseDTO.class));
+        }
+        return produtoResponse;
     }
 
-    public Produto obterPorId(Long id){
+    public ProdutoResponseDTO obterPorId(Long id){
         Optional<Produto> produto = produtoRepository.findById(id);
 
         if(produto.isEmpty()){
             throw new ResourceNotFound("Produto não foi encontrado na base com o Id: "+id);
         }
-        return produto.get();
+        return mapper.map(produto.get(), ProdutoResponseDTO.class);
     }
 
     //Update
-    public Produto atualizar(Long id, Produto produto){
+    public ProdutoResponseDTO atualizar(Long id, ProdutoRequestDTO produtoRequest){
         obterPorId(id);
-        produto.setId(id);
-        return produtoRepository.save(produto);
+
+        Produto produtoModel = mapper.map(produtoRequest, Produto.class);
+
+        produtoModel.setId(id);
+        produtoModel = produtoRepository.save(produtoModel);
+        
+        return mapper.map(produtoModel, ProdutoResponseDTO.class);
     }
 
     //Delete
